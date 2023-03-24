a48a7f347e71353f935d9cdc69dcee34973dbfa9
Seonu <seonup95@gmail.com>
2023-03-24 17:43:15 +0900

    feat: Create Payment page and Add routes

--- shopping-mall/src/routes.tsx
+++ shopping-mall/src/routes.tsx
@@ -1,6 +1,7 @@
 import GlobalLayout from "./pages/_layout";
 import Index from "./pages/index";
 import CartIndex from "./pages/cart/index";
+import PaymentIndex from "./pages/payment/index";
 import ProductsIndex from "./pages/products/index";
 import ProductsId from "./pages/products/[id]";
 
@@ -11,6 +12,7 @@ export const routes = [
     children: [
       { path: "/", element: <Index />, index: true },
       { path: "/cart", element: <CartIndex />, index: true },
+      { path: "/payment", element: <PaymentIndex />, index: true },
       { path: "/products", element: <ProductsIndex />, index: true },
       { path: "/products/:id", element: <ProductsId /> },
     ],
@@ -20,6 +22,7 @@ export const routes = [
 export const pages = [
   { route: "/" },
   { route: "/cart" },
+  { route: "/payment" },
   { route: "/products" },
   { route: "/products/:id" },
 ];
