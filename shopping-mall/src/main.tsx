f934fdc52cc78d66b1439bb933a4919848855b42
Seonu <seonup95@gmail.com>
2023-03-27 18:20:41 +0900

    conf: Set recoil

--- shopping-mall/src/main.tsx
+++ shopping-mall/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 import App from "./app";
 import { worker } from "./mocks/browser";
 
@@ -11,8 +12,10 @@ if (import.meta.env.DEV) {
 /** router 사용을 위해 애플리케이션 root를 BrowserRouter로 감싼다 */
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </RecoilRoot>
   </React.StrictMode>
 );
