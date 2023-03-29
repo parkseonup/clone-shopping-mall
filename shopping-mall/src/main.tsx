14f2bc8cdd13a1475bb030b70c77f6b25ea1ec73
Seonu <seonup95@gmail.com>
2023-03-29 23:47:38 +0900

    refact: Remove recoil root

--- shopping-mall/src/main.tsx
+++ shopping-mall/src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { RecoilRoot } from "recoil";
 import App from "./app";
 import { worker } from "./mocks/browser";
 
@@ -12,10 +11,8 @@ if (import.meta.env.DEV) {
 /** router 사용을 위해 애플리케이션 root를 BrowserRouter로 감싼다 */
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RecoilRoot>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </RecoilRoot>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
