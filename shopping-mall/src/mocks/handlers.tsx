f714c385e0e57f3f30061fd33efe2b65767b3622
Seonu <seonup95@gmail.com>
2023-03-29 23:41:12 +0900

    feat: Implement product's id with uuid library

--- shopping-mall/src/mocks/handlers.tsx
+++ shopping-mall/src/mocks/handlers.tsx
@@ -1,10 +1,11 @@
 import { graphql } from "msw";
+import { v4 as uuidv4 } from "uuid";
 import { ADD_CART, CartType, GET_CART } from "../graphql/cart";
 import { GET_PRODUCT, GET_PRODUCTS } from "../graphql/products";
 
 const mockProducts = (() =>
   Array.from({ length: 20 }).map((_, i) => ({
-    id: i + 1 + "",
+    id: uuidv4(),
     title: `임시제목${i + 1}`,
     imageUrl: `https://placeimg.com/200/150/nature/${i + 1}`,
     price: 50000,
