f31803c26d38cf6c21dbb093b5ef86299c6db6c0
Seonu <seonup95@gmail.com>
2023-04-05 14:29:39 +0900

    feat: Create to check cart's checkbox

--- shopping-mall/src/components/cart/item.tsx
+++ shopping-mall/src/components/cart/item.tsx
@@ -1,115 +1,121 @@
 import { useMutation } from "@tanstack/react-query";
-import { SyntheticEvent } from "react";
+import { ForwardedRef, forwardRef, SyntheticEvent } from "react";
 import { CartType, UPDATE_CART, DELETE_CART } from "../../graphql/cart";
 import { getClient, graphqlFetcher, QueryKeys } from "../../queryClient";
 
-const CartItem = ({ id, title, imageUrl, price, amount }: CartType) => {
-  const queryClient = getClient();
+const CartItem = forwardRef(
+  (
+    { id, title, imageUrl, price, amount }: CartType,
+    ref: ForwardedRef<HTMLInputElement>
+  ) => {
+    const queryClient = getClient();
 
-  const { mutate: updateCart } = useMutation(
-    ({ id, amount }: { id: string; amount: number }) =>
-      graphqlFetcher(UPDATE_CART, { id, amount }),
-    {
-      onMutate: async ({ id, amount }) => {
-        await queryClient.cancelQueries({ queryKey: [QueryKeys.CART] });
+    const { mutate: updateCart } = useMutation(
+      ({ id, amount }: { id: string; amount: number }) =>
+        graphqlFetcher(UPDATE_CART, { id, amount }),
+      {
+        onMutate: async ({ id, amount }) => {
+          await queryClient.cancelQueries({ queryKey: [QueryKeys.CART] });
 
-        const previousCart = queryClient.getQueryData<{
-          [key: string]: CartType;
-        }>([QueryKeys.CART]);
+          const previousCart = queryClient.getQueryData<{
+            [key: string]: CartType;
+          }>([QueryKeys.CART]);
 
-        if (!previousCart?.[id]) return previousCart;
+          if (!previousCart?.[id]) return previousCart;
 
-        const newCart = {
-          ...previousCart,
-          [id]: {
-            ...previousCart[id],
-            amount,
-          },
-        };
+          const newCart = {
+            ...previousCart,
+            [id]: {
+              ...previousCart[id],
+              amount,
+            },
+          };
 
-        queryClient.setQueryData([QueryKeys.CART], newCart);
+          queryClient.setQueryData([QueryKeys.CART], newCart);
 
-        return { previousCart };
-      },
-      onSuccess: (newCartItem) => {
-        const previousCart = queryClient.getQueryData<{
-          [key: string]: CartType;
-        }>([QueryKeys.CART]);
+          return { previousCart };
+        },
+        onSuccess: (newCartItem) => {
+          const previousCart = queryClient.getQueryData<{
+            [key: string]: CartType;
+          }>([QueryKeys.CART]);
 
-        const newCart = {
-          ...previousCart,
-          [id]: newCartItem,
-        };
+          const newCart = {
+            ...previousCart,
+            [id]: newCartItem,
+          };
 
-        queryClient.setQueryData([QueryKeys.CART], newCart);
-      },
+          queryClient.setQueryData([QueryKeys.CART], newCart);
+        },
+      }
+    );
+
+    const { mutate: deleteCart } = useMutation(
+      ({ id }: { id: string }) => graphqlFetcher(DELETE_CART, { id }),
+      {
+        onMutate: async ({ id }) => {
+          await queryClient.cancelQueries({ queryKey: [QueryKeys.CART] });
+          const previousCart = queryClient.getQueryData<{
+            [key: string]: CartType;
+          }>([QueryKeys.CART]);
+
+          const newCart = { ...previousCart };
+          delete newCart[id];
+
+          queryClient.setQueryData([QueryKeys.CART], newCart);
+
+          return { previousCart };
+        },
+        onSuccess: () => {
+          const previousCart = queryClient.getQueryData<{
+            [key: string]: CartType;
+          }>([QueryKeys.CART]);
+
+          const newCart = { ...previousCart };
+          delete newCart[id];
+
+          queryClient.setQueryData([QueryKeys.CART], newCart);
+        },
+      }
+    );
+
+    function handleUpdateAmount(e: SyntheticEvent) {
+      const amount = +(e.target as HTMLInputElement).value;
+      updateCart({ id, amount });
     }
-  );
 
-  const { mutate: deleteCart } = useMutation(
-    ({ id }: { id: string }) => graphqlFetcher(DELETE_CART, { id }),
-    {
-      onMutate: async ({ id }) => {
-        await queryClient.cancelQueries({ queryKey: [QueryKeys.CART] });
-        const previousCart = queryClient.getQueryData<{
-          [key: string]: CartType;
-        }>([QueryKeys.CART]);
-
-        const newCart = { ...previousCart };
-        delete newCart[id];
-
-        queryClient.setQueryData([QueryKeys.CART], newCart);
-
-        return { previousCart };
-      },
-      onSuccess: () => {
-        const previousCart = queryClient.getQueryData<{
-          [key: string]: CartType;
-        }>([QueryKeys.CART]);
-
-        const newCart = { ...previousCart };
-        delete newCart[id];
-
-        queryClient.setQueryData([QueryKeys.CART], newCart);
-      },
+    function handleDeleteCart() {
+      deleteCart({ id });
     }
-  );
 
-  function handleUpdateAmount(e: SyntheticEvent) {
-    const amount = +(e.target as HTMLInputElement).value;
-    updateCart({ id, amount });
+    return (
+      <li className="cart-item">
+        <input
+          type="checkbox"
+          className="cart-item__checkbox"
+          name="select-item"
+          ref={ref}
+        />
+        <p className="cart-item__title">{title}</p>
+        <img src={imageUrl} alt="" />
+        <p className="cart-item__price">{price}</p>
+        <input
+          type="number"
+          className="cart-item__amount"
+          value={amount}
+          onChange={handleUpdateAmount}
+          min={1}
+        />
+        <button
+          type="button"
+          className="cart-item__delete"
+          onClick={handleDeleteCart}
+        >
+          삭제
+        </button>
+      </li>
+    );
   }
-
-  function handleDeleteCart() {
-    deleteCart({ id });
-  }
-
-  return (
-    <li className="cart-item">
-      <input
-        type="checkbox"
-        className="cart-item__checkbox"
-        name="select-item"
-      />
-      <p className="cart-item__title">{title}</p>
-      <img src={imageUrl} alt="" />
-      <p className="cart-item__price">{price}</p>
-      <input
-        type="number"
-        className="cart-item__amount"
-        value={amount}
-        onChange={handleUpdateAmount}
-        min={1}
-      />
-      <button
-        type="button"
-        className="cart-item__delete"
-        onClick={handleDeleteCart}
-      >
-        삭제
-      </button>
-    </li>
-  );
-};
+);
 
 export default CartItem;
--- shopping-mall/src/components/cart/list.tsx
+++ shopping-mall/src/components/cart/list.tsx
@@ -1,17 +1,53 @@
+import { SyntheticEvent, useRef } from "react";
 import { CartType } from "../../graphql/cart";
 import CartItem from "./item";
 
-const CartList = ({ items }: { items: CartType[] }) => (
-  <form>
-    <label>
-      <input type="checkbox" className="select-all" name="select-all" />
-    </label>
-    <ul className="cart">
-      {items.map((item) => (
-        <CartItem {...item} key={item.id} />
-      ))}
-    </ul>
-  </form>
-);
+// TODO: 강사님 코드로 다시 짜보고 어떤게 더 좋은지 고민해보기
+// TODO: ref의 매개변수로 node가 등록되었을 때 node가 undefined일 경우가 어떻게 생기는지 알아보기
+const CartList = ({ items }: { items: CartType[] }) => {
+  const cartAllcheckboxRef = useRef<HTMLInputElement>(null);
+  const cartItemCheckboxRef = useRef(new Map());
+
+  function handleChangeCheckbox(e: SyntheticEvent) {
+    if (e.target === cartAllcheckboxRef.current) {
+      cartItemCheckboxRef.current.forEach(
+        (checkbox) => (checkbox.checked = cartAllcheckboxRef.current!.checked)
+      );
+    } else {
+      const checkedItemCount = [...cartItemCheckboxRef.current.values()].filter(
+        (checkbox) => checkbox.checked
+      ).length;
+
+      cartAllcheckboxRef.current!.checked =
+        checkedItemCount === items.length ? true : false;
+    }
+  }
+
+  return (
+    <form onChange={handleChangeCheckbox}>
+      <label>
+        <input
+          type="checkbox"
+          className="select-all"
+          name="select-all"
+          ref={cartAllcheckboxRef}
+        />
+      </label>
+      <ul className="cart">
+        {items.map((item) => (
+          <CartItem
+            {...item}
+            key={item.id}
+            ref={(node: HTMLInputElement) => {
+              node
+                ? cartItemCheckboxRef.current.set(item.id, node)
+                : cartItemCheckboxRef.current.delete(item.id);
+            }}
+          />
+        ))}
+      </ul>
+    </form>
+  );
+};
 
 export default CartList;
