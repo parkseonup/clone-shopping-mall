fa290e2b8625c3a586142a895a4e48688340c5ec
Seonu <seonup95@gmail.com>
2023-04-07 23:54:38 +0900

    refact: Change dummy image url

--- shopping-mall/client/src/mocks/handlers.tsx
+++ shopping-mall/client/src/mocks/handlers.tsx
@@ -13,7 +13,7 @@ const mockProducts = (() =>
   Array.from({ length: 20 }).map((_, i) => ({
     id: i + 1 + "",
     title: `임시제목${i + 1}`,
-    imageUrl: `https://placeimg.com/200/150/nature/${i + 1}`,
+    imageUrl: `https://picsum.photos/id/${i + 20}/200/150`,
     price: 50000,
     description: `임시설명${i + 1}`,
     createdAt: new Date(1661646735501883 + i * 1000 * 60 * 60 * 24).toString(),
