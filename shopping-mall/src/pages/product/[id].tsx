eec6f1c9f7ed5f771609eb07d58d6f89ea6ad618
Seonu <seonup95@gmail.com>
2023-05-17 17:51:08 +0900

    refact: Rename product dir to products dir

--- shopping-mall/src/pages/product/[id].tsx
+++
@@ -1,9 +0,0 @@
-function ProductDetailPage() {
-  return (
-    <>
-      <h2>상품 상세 페이지</h2>
-    </>
-  );
-}
-
-export default ProductDetailPage;
--- shopping-mall/src/pages/product/index.tsx
+++
@@ -1,9 +0,0 @@
-function ProductsPage() {
-  return (
-    <>
-      <h2>상품 목록 페이지</h2>
-    </>
-  );
-}
-
-export default ProductsPage;
