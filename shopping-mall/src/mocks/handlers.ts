eeb14fa804fb341fa58b706f4be070441d8ff1eb
Seonu <seonup95@gmail.com>
2023-05-18 13:56:01 +0900

    feat: Create mock cart data & cart handlers

--- shopping-mall/src/mocks/handlers.ts
+++ shopping-mall/src/mocks/handlers.ts
@@ -1,21 +1,21 @@
 import { graphql } from 'msw';
-import { mockProducts } from './mockData';
+import { mockProducts, mockCart } from './mockData';
 
 export const handlers = [
   // product
   graphql.query('GET_PRODUCTS', (req, res, ctx) => {
-    return res(ctx.data(mockProducts));
+    return res(ctx.data({ products: mockProducts }));
   }),
   graphql.query('GET_PRODUCT', ({ variables }, res, ctx) => {
     const product = mockProducts.find(product => product.id === variables.id);
 
     if (!product) throw new Error(`상품(${variables.id})이 존재하지 않습니다.`);
 
-    return res(ctx.data(product));
+    return res(ctx.data({ product }));
   }),
   graphql.query('ADD_PRODUCT', ({ variables }, res, ctx) => {
     const { title, imageUrl, description, price } = variables;
-    const id = +mockProducts.at(-1)!.id + 1 + '';
+    const id = !mockProducts.at(-1) ? '1' : +mockProducts.at(-1)!.id + 1 + '';
     const newProduct = {
       id,
       title,
@@ -27,7 +27,7 @@ export const handlers = [
 
     mockProducts.push(newProduct);
 
-    return res(ctx.data(newProduct));
+    return res(ctx.data({ newProduct }));
   }),
   graphql.query('UPDATE_PRODUCT', ({ variables }, res, ctx) => {
     const targetProduct = mockProducts.find(
@@ -42,7 +42,7 @@ export const handlers = [
       ...variables,
     };
 
-    return res(ctx.data(updatedProduct));
+    return res(ctx.data({ updatedProduct }));
   }),
   graphql.query('DELETE_PRODUCT', ({ variables }, res, ctx) => {
     const targetIndex = mockProducts.findIndex(
@@ -54,25 +54,87 @@ export const handlers = [
 
     mockProducts.splice(targetIndex, 1);
 
-    return res(ctx.data(variables.id));
+    return res(ctx.data({ deletedProductId: variables.id }));
   }),
 
   // cart
   graphql.query('GET_CART', (req, res, ctx) => {
-    // return res(ctx.data("..."));
+    return res(ctx.data({ cart: mockCart }));
   }),
-  graphql.query('ADD_CART', (req, res, ctx) => {
-    // return res(ctx.data("..."));
+  graphql.query('ADD_CART', ({ variables }, res, ctx) => {
+    const targetProduct = mockProducts.find(
+      product => product.id === variables.productId
+    );
+
+    if (!targetProduct)
+      throw new Error(`상품(${variables.productId}})이 존재하지 않습니다.`);
+    if (!targetProduct.createdAt)
+      throw new Error(`상품(${variables.productId}})은 품절되었습니다.`);
+
+    const targetCart = mockCart.find(cart => cart.product === targetProduct);
+    let newCartItem = null;
+
+    if (targetCart) {
+      newCartItem = {
+        ...targetCart,
+        amount: targetCart.amount + 1,
+      };
+    } else {
+      const id = !mockCart.at(-1) ? '1' : +mockCart.at(-1)!.id + 1 + '';
+      newCartItem = {
+        id,
+        amount: 1,
+        product: targetProduct,
+      };
+    }
+
+    mockCart.push(newCartItem);
+
+    return res(ctx.data({ newCartItem }));
   }),
-  graphql.query('UPDATE_CART', (req, res, ctx) => {
-    // return res(ctx.data("..."));
+  graphql.query('UPDATE_CART', ({ variables }, res, ctx) => {
+    const { cartId, amount } = variables;
+    const targetIndex = mockCart.findIndex(cart => cart.id === cartId);
+
+    if (targetIndex < 0)
+      throw new Error(
+        `장바구니에 상품(${variables.cartId}})이 존재하지 않습니다.`
+      );
+
+    const newCartItem = {
+      ...mockCart[targetIndex],
+      amount,
+    };
+
+    mockCart[targetIndex] = newCartItem;
+
+    return res(ctx.data({ newCartItem }));
   }),
-  graphql.query('DELETE_CART', (req, res, ctx) => {
-    // return res(ctx.data("..."));
+  graphql.query('DELETE_CART', ({ variables }, res, ctx) => {
+    const targetIndex = mockCart.findIndex(
+      cart => cart.id === variables.cartId
+    );
+
+    mockCart.splice(targetIndex, 1);
+    return res(ctx.data({ deletedCartId: variables.cartId }));
   }),
 
   // payment
-  graphql.query('EXECUTE_PAY', (req, res, ctx) => {
-    // return res(ctx.data("..."));
+  graphql.query('EXECUTE_PAY', ({ variables }, res, ctx) => {
+    const { ids } = variables; // cartId[]
+
+    const deletedIds = ids.filter((id: string) => {
+      const targetCartIndex = mockCart.findIndex(cart => cart.id === id);
+
+      if (!mockCart[targetCartIndex].product.createdAt)
+        throw new Error(
+          `결제 목록에 품절된 상품(${variables.id}})이 포함되어 있습니다.`
+        );
+
+      mockCart.splice(targetCartIndex, 1);
+      return true;
+    });
+
+    return res(ctx.data({ deletedIds }));
   }),
 ];
--- shopping-mall/src/mocks/mockData.ts
+++ shopping-mall/src/mocks/mockData.ts
@@ -1,8 +1,28 @@
-export const mockProducts = Array.from({ length: 60 }, (_, i) => ({
-  id: i + 1 + '',
-  title: `제목_${i + 1}`,
-  imageUrl: `https://picsum.photos/id/${i + 30}/200/300.jpg`,
-  price: 3000,
-  description: `설명_${i + 1}`,
-  createdAt: Date.now(),
-}));
+import { CartType, ProductsType } from '../types';
+
+export const mockProducts: ProductsType = Array.from(
+  { length: 60 },
+  (_, i) => ({
+    id: i + 1 + '',
+    title: `제목_${i + 1}`,
+    description: `설명_${i + 1}`,
+    imageUrl: `https://picsum.photos/id/${i + 1 + 30}/200/300.jpg`,
+    price: 3000,
+    createdAt: Date.now(),
+  })
+);
+
+export const mockCart: CartType = [
+  {
+    id: '1',
+    amount: 3,
+    product: {
+      id: '2',
+      title: '제목_2',
+      description: '설명_2',
+      imageUrl: 'https://picsum.photos/id/32/200/300.jpg',
+      price: 3000,
+      createdAt: 1684381913374,
+    },
+  },
+];
