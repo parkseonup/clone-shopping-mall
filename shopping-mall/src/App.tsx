2eb665ac89aa1380e47266219051e89719ab8bb6
Seonu <seonup95@gmail.com>
2023-05-26 01:32:43 +0900

    conf: Set recoil

--- shopping-mall/src/App.tsx
+++ shopping-mall/src/App.tsx
@@ -3,15 +3,18 @@ import { routes } from './route';
 import Gnb from './components/gnb';
 import { QueryClientProvider } from 'react-query';
 import { queryClient } from './fetcher';
+import { RecoilRoot } from 'recoil';
 
 function App() {
   const element = useRoutes(routes);
 
   return (
     <QueryClientProvider client={queryClient}>
-      <h1>Shopping Mall</h1>
-      <Gnb />
-      {element}
+      <RecoilRoot>
+        <h1>Shopping Mall</h1>
+        <Gnb />
+        {element}
+      </RecoilRoot>
     </QueryClientProvider>
   );
 }
